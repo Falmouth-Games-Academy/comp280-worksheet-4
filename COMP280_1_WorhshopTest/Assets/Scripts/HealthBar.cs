de4a5b4e1e567bc10e144677fc7803cccb8582ec
mo214885 <mo214885@falmouth.ac.uk>
2019-12-09 10:06:46 +0000

    Adding unity project to the repository
    Signed-off-by: mo214885 <mo214885@falmouth.ac.uk>

---
+++ COMP280_1_WorhshopTest/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField]
+    Image bar;
+    [SerializeField]
+    float damage;
+
+    float health = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Damage", 2.0f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Damage() 
+    {
+        health -= damage;
+        bar.fillAmount = (health / 100);
+    }
+}
---
+++ COMP280_1_WorhshopTest/Assets/Scripts/Menu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class Menu : MonoBehaviour
+{
+    [SerializeField]
+    GameObject menu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+            menu.SetActive(true);
+    }
+
+    public void Resume() 
+    {
+        menu.SetActive(false);
+    }
+
+    public void Quit() 
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
