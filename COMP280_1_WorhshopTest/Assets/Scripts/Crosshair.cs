3d24d7f51e0b2317a161edbf4e403802e2b70285
mo214885 <mo214885@falmouth.ac.uk>
2019-12-11 14:37:47 +0000

    Added standard assets to the build, added a script for the crosshair to make it change colour when its in range of an enemy and added an enemy script that will destroy the enemy when triggered
    Signed-off-by: mo214885 <mo214885@falmouth.ac.uk>

---
+++ COMP280_1_WorhshopTest/Assets/Scripts/Crosshair.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Crosshair : MonoBehaviour
+{
+    [SerializeField]
+    Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+
+        if (Physics.Raycast(ray, out hit)) 
+        {
+            if (hit.transform.name.Contains("Enemy"))
+            {
+                image.color = new Color(1, 0, 0, 1);
+
+                if (Input.GetMouseButtonDown(0)) 
+                {
+                    hit.transform.GetComponent<Enemy>().KillEnemy();
+                }
+            }
+        }
+        else
+            image.color = new Color(1, 1, 1, 1);
+    }
+}
